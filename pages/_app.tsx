87b423f82fca3a4b46064d09317fa5f252283956
TexasMade10 <sales@pinecrestconsulting.com>
2025-06-26 23:40:25 -0500

    Ensure all homepage and config files are present

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
-import App from '../ui/App'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <App />
+  return <Component {...pageProps} />
 } 
